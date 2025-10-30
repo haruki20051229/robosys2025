a11f3ef34c67487a38d2a6c0f2975fbb3032af4c
haruki20051229 <s24c1019sq@s.chibakoudai.jp>
2025-10-30 13:22:49 +0900

    a

--- test.bash
+++ test.bash
@@ -1,13 +1,15 @@
 #!/bin/bash
-
+# SPDX-FileCopyrightext: 2025 Haruki Ueda
+# SPDX-License-Identifier: BSD-3-Clause
 ng () {
-        eecho ${1}行目が違うよ
+        echo ${1}行目が違うよ
         res=1
 }
 
 res=0
-a=山田
-[ "$a" = 上田 ] || ng "$LINENO"
-[ "$a" = 山田 ] || ng "$LINENO"
 
+out=$(seq 5 | ./plus)
+[ "${out}" = 15 ] || ng "$LINENO"
+
+[ "${res}" = 0 ] && echo OK
 exit $res
