e61b4998b9784e58ae290459a1fab73083afb963
haruki20051229 <s24c1019sq@s.chibakoudai.jp>
2025-10-30 13:28:49 +0900

    更新

--- test.bash
+++ test.bash
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash -xv
 # SPDX-FileCopyrightext: 2025 Haruki Ueda
 # SPDX-License-Identifier: BSD-3-Clause
 ng () {
@@ -10,6 +10,14 @@ res=0
 
 out=$(seq 5 | ./plus)
 [ "${out}" = 15 ] || ng "$LINENO"
+
+out=$(echo あ| ./plus)
+[ "$?" = 1 ]      || ng "$LINENO"
+[ "${out}" = "" ] || ng "$LINENO"
+
+out=$(echo | ./plus)
+[ "$?" = 1 ]      || ng "$LINENO"
+[ "${out}" = "" ] || ng "$LINENO"
 
 [ "${res}" = 0 ] && echo OK
 exit $res
